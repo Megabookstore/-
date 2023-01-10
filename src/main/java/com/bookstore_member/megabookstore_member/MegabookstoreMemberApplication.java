bbe7990dd66c293f0c7eb47c0d9e88e02d8dbb67
seulhui <75656859+leeseulhui@users.noreply.github.com>
2023-01-10 16:24:00 +0900

    회원

---
+++ src/main/java/com/bookstore_member/megabookstore_member/MegabookstoreMemberApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore_member.megabookstore_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegabookstoreMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegabookstoreMemberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstore_member/megabookstore_member/MegabookstoreMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore_member.megabookstore_member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegabookstoreMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
