b0a9fb07d838d1f495a4576adba6cd0bf84dca68
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2023-01-10 19:38:27 +0900

    feat

---
+++ src/main/java/com/bookstore_member/megabookstore_member/domain/Member.java
@@ -0,0 +1,51 @@
+package com.bookstore_member.megabookstore_member.domain;
+
+import com.bookstore_member.megabookstore_member.domain.converter.BooleanToYNConverter;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_no")
+    private Long memberNo;
+
+    @Column(name = "membershipNo")
+    private Long membershipNo;
+
+    private String id;
+
+    @Column(name = "nickName")
+    private String nickName;
+
+    @Column(name = "is_man")
+    @Convert(converter = BooleanToYNConverter.class)
+    private boolean isMan;
+
+    private LocalDateTime birth;
+
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    private String email;
+
+    @Column(name = "member_created_at")
+    private String memberCreatedAt;
+
+//    @OneToMany
+//    private List<MemberInquiry> inquiries;
+
+}
---
+++ src/main/java/com/bookstore_member/megabookstore_member/domain/converter/BooleanToYNConverter.java
@@ -0,0 +1,22 @@
+package com.bookstore_member.megabookstore_member.domain.converter;
+
+import javax.persistence.AttributeConverter;
+
+public class BooleanToYNConverter implements AttributeConverter<Boolean,String> {
+
+    /**
+     * Boolean 값을 Y 또는 N 으로 convert!
+     *
+     * @param attribute  boolean 값
+     * @return String true  인 경우, Y 또는 false 인 경우 N
+     */
+    @Override
+    public String convertToDatabaseColumn(Boolean attribute) {
+        return (attribute!=null && attribute)? "Y" : "N";
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String yn) {
+        return "Y".equalsIgnoreCase(yn);
+    }
+}
