e2f88f269541980a1ffb6c3bb9a96abae285abf5
SSAFY <xodnjs8287@naver.com>
2023-01-12 15:27:50 +0900

    feat : member 유효성 check

---
+++ src/main/java/com/bookstore_member/megabookstore_member/domain/member/valid/NickName.java
@@ -0,0 +1,40 @@
+package com.bookstore_member.megabookstore_member.domain.member.valid;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class NickName {
+
+    private static final String NICKNAME_NOT_EMPTY = "닉네임은 빈값을 허용하지 않습니다";
+
+
+    @Column(nullable = false, unique = true)
+    private String nickName;
+
+    public NickName(String nickName){
+        validate(nickName);
+        this.nickName = nickName;
+    }
+
+    private void validate (String nickName){
+        validateNickNameNotEmpty(nickName);
+    }
+
+    private void validateNickNameNotEmpty(String nickName){
+        if(Objects.isNull(nickName) || nickName.isEmpty()){
+            throw new IllegalArgumentException(NICKNAME_NOT_EMPTY);
+        }
+    }
+
+
+}
---
+++ src/main/java/com/bookstore_member/megabookstore_member/domain/member/valid/PhoneNumber.java
@@ -0,0 +1,41 @@
+package com.bookstore_member.megabookstore_member.domain.member.valid;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class PhoneNumber {
+
+    private static final String PHONE_NUMBER_NOT_EMPTY = "휴대폰 번호는 빈값을 허용하지 않습니다";
+
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    public PhoneNumber(String phoneNumber){
+        validate(phoneNumber);
+        this.phoneNumber = phoneNumber;
+    }
+
+    private void validate (String phoneNumber){
+        validateNickNameNotEmpty(phoneNumber);
+    }
+
+    private void validateNickNameNotEmpty(String phoneNumber){
+        if(Objects.isNull(phoneNumber) || phoneNumber.isEmpty()){
+            throw new IllegalArgumentException(PHONE_NUMBER_NOT_EMPTY);
+        }
+    }
+
+
+
+
+}
