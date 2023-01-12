52823927aae93343969d40e9c782e9461ef43c2b
SSAFY <xodnjs8287@naver.com>
2023-01-12 15:30:54 +0900

    feat : MemberService

--- src/main/java/com/bookstore_member/megabookstore_member/service/MemberService.java
+++ src/main/java/com/bookstore_member/megabookstore_member/service/MemberService.java
@@ -1,26 +1,31 @@
 package com.bookstore_member.megabookstore_member.service;
 
-import com.bookstore_member.megabookstore_member.domain.Member;
+import com.bookstore_member.megabookstore_member.domain.member.Member;
 import com.bookstore_member.megabookstore_member.dto.MemberRequest;
 import com.bookstore_member.megabookstore_member.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.naming.ldap.PagedResultsControl;
 
 
 @Service
 @AllArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
     private MemberRepository memberRepository;
 
-    private Long registerMember(MemberRequest memberRequest) {
+    @Transactional
+    public Long registerMember(MemberRequest memberRequest) {
         Member member = Member.builder()
                 .memberNo(memberRequest.getMemberNo())
                 .membershipNo(memberRequest.getMembershipNo())
                 .memberCreatedAt(memberRequest.getMemberCreatedAt())
                 .birth(memberRequest.getBirth())
                 .email(memberRequest.getEmail())
-                .id(memberRequest.getId())
+                .memberId(memberRequest.getId())
                 .nickName(memberRequest.getNickName())
                 .isMan(memberRequest.isMan())
                 .password(memberRequest.getPassword())
@@ -28,5 +33,21 @@ public class MemberService {
 
         return member.getMemberNo();
     }
+
+    @Transactional
+    public boolean validateMemberIdDuplicate(String memberId){
+        return memberRepository.existsByMemberId(memberId);
+    }
+
+    @Transactional
+    public boolean validateEmailDuplicate(String email){
+        return memberRepository.existsByEmail(email);
+    }
+
+    @Transactional
+    public boolean validatePhoneNumberDuplicate(String phoneNumber){
+        return memberRepository.existsByPhoneNumber(phoneNumber);
+    }
+
 
 }
