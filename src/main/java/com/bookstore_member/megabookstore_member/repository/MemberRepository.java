6cfc3f5a204fee8c8b84601b089315a127fa330a
SSAFY <xodnjs8287@naver.com>
2023-01-12 15:31:59 +0900

    feat : Member repository

--- src/main/java/com/bookstore_member/megabookstore_member/repository/MemberRepository.java
+++ src/main/java/com/bookstore_member/megabookstore_member/repository/MemberRepository.java
@@ -1,8 +1,12 @@
 package com.bookstore_member.megabookstore_member.repository;
 
-import com.bookstore_member.megabookstore_member.domain.Member;
+import com.bookstore_member.megabookstore_member.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByMemberId(String memberId);
 
+    boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
 }
