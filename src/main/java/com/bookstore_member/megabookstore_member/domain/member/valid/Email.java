f4ec83b29f97e92e02f5fae7a68843f706159966
SSAFY <xodnjs8287@naver.com>
2023-01-12 15:27:50 +0900

    feat : member 유효성 check

---
+++ src/main/java/com/bookstore_member/megabookstore_member/domain/member/valid/Email.java
@@ -0,0 +1,38 @@
+package com.bookstore_member.megabookstore_member.domain.member.valid;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class Email {
+
+    private static final String EMAIL_NOT_EMPTY = "닉네임은 빈값을 허용하지 않습니다";
+
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public Email(String email){
+        validate(email);
+        this.email = email;
+    }
+
+    private void validate (String email){
+        validateEmailNotEmpty(email);
+    }
+
+    private void validateEmailNotEmpty(String email){
+        if(Objects.isNull(email) || email.isEmpty()){
+            throw new IllegalArgumentException(EMAIL_NOT_EMPTY);
+        }
+    }
+
+}
---
+++ src/main/java/com/bookstore_member/megabookstore_member/domain/member/valid/MemberId.java
@@ -0,0 +1,41 @@
+package com.bookstore_member.megabookstore_member.domain.member.valid;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+@Embeddable
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class MemberId {
+
+    private static final String ID_NOT_EMPTY = "아이디는 빈값을 허용하지 않습니다";
+
+    private static final String ID_NOT_DUPLICATED = "아이디는 중복을 허용하지 않습니다";
+
+    @Column(nullable = false, unique = true)
+    private String memberId;
+    public MemberId(String memberId){
+        validate(memberId);
+        this.memberId = memberId;
+    }
+
+    private void validate (String memberId){
+        validateMemberIdNotEmpty(memberId);
+    }
+
+    private void validateMemberIdNotEmpty(String memberId){
+        if(Objects.isNull(memberId) || memberId.isEmpty()){
+            throw new IllegalArgumentException(ID_NOT_EMPTY);
+        }
+    }
+
+}
