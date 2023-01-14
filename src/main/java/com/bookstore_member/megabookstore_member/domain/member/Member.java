701811e0fa30c214087b3221866c3ba74e967952
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2023-01-14 16:07:53 +0900

    fix

--- src/main/java/com/bookstore_member/megabookstore_member/domain/member/Member.java
+++ src/main/java/com/bookstore_member/megabookstore_member/domain/member/Member.java
@@ -1,8 +1,13 @@
 package com.bookstore_member.megabookstore_member.domain.member;
 
-import com.bookstore_member.megabookstore_member.domain.MemberInquiry;
 import com.bookstore_member.megabookstore_member.domain.converter.BooleanToYNConverter;
+import com.bookstore_member.megabookstore_member.domain.member.valid.Email;
+import com.bookstore_member.megabookstore_member.domain.member.valid.MemberId;
+import com.bookstore_member.megabookstore_member.domain.member.valid.NickName;
+import com.bookstore_member.megabookstore_member.domain.member.valid.PhoneNumber;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -12,7 +17,7 @@ import java.util.List;
 @Entity
 @Data
 @Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
 public class Member {
 
@@ -25,31 +30,39 @@ public class Member {
     private Long membershipNo;
 
     @Embedded
-    private String memberId;
+    private MemberId memberId;
 
     @Embedded
-    private String nickName;
+    private NickName nickName;
 
     @Column(nullable = false)
     @Convert(converter = BooleanToYNConverter.class)
     private boolean isMan;
 
     @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDateTime birth;
 
     @Column(nullable = false)
     private String password;
 
     @Embedded
-    private String phoneNumber;
+    private PhoneNumber phoneNumber;
 
     @Embedded
-    private String email;
+    private Email email;
 
     @Column(nullable = false)
-    private String memberCreatedAt;
+    @CreatedDate
+    private LocalDateTime memberCreatedAt;
 
-    @OneToMany
-    private List<MemberInquiry> inquiries = new ArrayList<>();
+
+    public void updateMember(MemberId memberId, NickName nickName, String password , PhoneNumber phoneNumber, Email email){
+        this.memberId = memberId;
+        this.nickName = nickName;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 
 }
