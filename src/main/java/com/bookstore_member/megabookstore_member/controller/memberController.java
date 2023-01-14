f7abe4185b57bd5084dd2cb835b927e136a6e919
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2023-01-14 16:08:47 +0900

    feat : RestController 회원등록

--- src/main/java/com/bookstore_member/megabookstore_member/controller/memberController.java
+++ src/main/java/com/bookstore_member/megabookstore_member/controller/memberController.java
@@ -1,12 +1,26 @@
 package com.bookstore_member.megabookstore_member.controller;
 
+import com.bookstore_member.megabookstore_member.dto.MemberRequest;
+import com.bookstore_member.megabookstore_member.dto.MemberResponse;
 import com.bookstore_member.megabookstore_member.service.MemberService;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@AllArgsConstructor
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
 public class memberController {
 
-    private MemberService memberService;
+    private final MemberService memberService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public MemberResponse registerMember(
+            @RequestBody MemberRequest memberRequest) {
+        return memberService.registerMember(memberRequest);
+
+    }
+
+
 }
