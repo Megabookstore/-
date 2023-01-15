55669f3d8cd79e868f9e798c62be259cc1cf663f
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2023-01-15 20:59:56 +0900

    feat : 멤버쉽 이력 엔티티 생성

---
+++ src/main/java/com/bookstore_member/megabookstore_member/domain/membership_history/MembershipHistory.java
@@ -0,0 +1,40 @@
+package com.bookstore_member.megabookstore_member.domain.membership_history;
+
+
+import com.bookstore_member.megabookstore_member.domain.member.Member;
+import com.bookstore_member.megabookstore_member.domain.membership_history.validation.MonthlyUsageAmount;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MembershipHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long membershipHistoryNo;
+
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    //private Long membershipNo;
+
+    @Embedded
+    private MonthlyUsageAmount monthlyUsageAmount;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime membershipHistoryCreateAt;
+
+
+}
