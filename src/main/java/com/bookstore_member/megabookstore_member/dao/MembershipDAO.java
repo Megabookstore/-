909f147c604f986c700f64c61459406d3703c838
seulhui <75656859+leeseulhui@users.noreply.github.com>
2023-01-16 21:26:36 +0900

    Refactor : 회원 Dto 및 회원 서비스 분리

--- src/main/java/com/bookstore_member/megabookstore_member/dao/MembershipDAO.java
+++
@@ -1,8 +0,0 @@
-package com.bookstore_member.megabookstore_member.dao;
-
-import com.bookstore_member.megabookstore_member.entity.MembershipEntity;
-
-public interface MembershipDAO {
-    MembershipEntity saveMembership(MembershipEntity membershipEntity); //membership 저장
-    MembershipEntity getMembership(String membershipId); //membership 조회
-}
--- src/main/java/com/bookstore_member/megabookstore_member/repository/MembershipRepository.java
+++ src/main/java/com/bookstore_member/megabookstore_member/repository/MembershipRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.persistence.criteria.CriteriaBuilder;
 
-public interface MembershipRepository extends JpaRepository<MembershipEntity, String> {
+public interface MembershipRepository extends JpaRepository<MembershipEntity, Long> {
+
+
 }
