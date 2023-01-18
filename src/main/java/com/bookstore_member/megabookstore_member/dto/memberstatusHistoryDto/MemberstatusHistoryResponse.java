53ff52e574c54c13420d860456f71149e128b0e9
seulhui <75656859+leeseulhui@users.noreply.github.com>
2023-01-18 21:49:21 +0900

    feat : 회원 상태 이력 Dto 등록

---
+++ src/main/java/com/bookstore_member/megabookstore_member/dto/memberstatusHistoryDto/MemberstatusHistoryResponse.java
@@ -0,0 +1,34 @@
+package com.bookstore_member.megabookstore_member.dto.memberstatusHistoryDto;
+
+import com.bookstore_member.megabookstore_member.domain.member.Member;
+import com.bookstore_member.megabookstore_member.domain.member_status_history.MemberstatusHistory;
+import com.bookstore_member.megabookstore_member.domain.member_status_history.validation.StatusChangedReason;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class MemberstatusHistoryResponse {
+    private Long memberstatusHistoryNo;
+    private Member member;
+    private Long memberstatusNo;
+    private LocalDateTime memberstatusHistoryCreatedAt;
+    private StatusChangedReason statusChangedReason;
+
+    public MemberstatusHistoryResponse(MemberstatusHistory memberstatusHistory){
+        this.memberstatusHistoryNo = memberstatusHistory.getMemberstatusHistoryNo();
+        this.member = memberstatusHistory.getMember();
+        this.memberstatusNo = memberstatusHistory.getMemberstatusNo();
+        this.memberstatusHistoryCreatedAt = memberstatusHistory.getMemberstatusHistoryCreatedAt();
+        this.statusChangedReason = StatusChangedReason(statusChangedReason);
+    }
+
+    public StatusChangedReason statusChangedReason(MemberstatusHistory memberstatusHistory) {
+        return memberstatusHistory.getStatusChangedReason();
+
+    }
+}
