fa8e531dfba9ea4313b17eae5204ac7977716161
seulhui <75656859+leeseulhui@users.noreply.github.com>
2023-01-18 21:49:21 +0900

    feat : 회원 상태 이력 Dto 등록

---
+++ src/main/java/com/bookstore_member/megabookstore_member/dto/memberstatusHistoryDto/MemberstatusHistoryRequest.java
@@ -0,0 +1,31 @@
+package com.bookstore_member.megabookstore_member.dto.memberstatusHistoryDto;
+
+import com.bookstore_member.megabookstore_member.domain.member.Member;
+import com.bookstore_member.megabookstore_member.domain.member_status_history.MemberstatusHistory;
+import com.bookstore_member.megabookstore_member.domain.member_status_history.validation.StatusChangedReason;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class MemberstatusHistoryRequest {
+    private Long memberstatusHistoryNo;
+    private Member member;
+    private Long memberstatusNo;
+    private Long statusChangedReason;
+    private LocalDateTime memberstatusHistoryCreatedAt;
+    public MemberstatusHistory tomemberstatusHistory(){
+        return MemberstatusHistory.builder()
+                .memberstatusHistoryNo(memberstatusHistoryNo)
+                .member(member)
+                .memberstatusHistoryCreatedAt(memberstatusHistoryCreatedAt)
+                .statusChangedReason(new StatusChangedReason(statusChangedReason))
+                .build();
+    }
+
+
+}
